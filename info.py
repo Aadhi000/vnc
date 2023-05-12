6ce118e092c71124c226fc92821a7c0bbc4d16f1
ααdhí <87893573+Aadhi000@users.noreply.github.com>
2023-05-12 22:06:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -16,6 +16,9 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
+CLONE_SESSIONS = (environ.get('CLONE_SESSIONS'))
+
+CLONED_SESSIONS = (environ.get('CLONED_SESSIONS'))
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
