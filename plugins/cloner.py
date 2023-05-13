30f06ed65f75872ab8ece47990a83ff0cce50f47
ααdhí <87893573+Aadhi000@users.noreply.github.com>
2023-05-13 15:04:18 +0530

    Update cloner.py

--- plugins/cloner.py
+++ plugins/cloner.py
@@ -166,7 +166,7 @@ async def cloned_count(client, message):
         
         @Client.on_message(filters.command(["removebot"]) & filters.user(ADMINS))
 
-async def remove_bot(Client: message):
+async def remove_bot(client, message):
 
     bot_username = message.text.split(" ", maxsplit=1)[1].strip()
 
